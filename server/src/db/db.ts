4cbda4b46d076ab45fcfa839f8076722a554eae4
Kamenow <iavorkamenov777@gmail.com>
2024-04-30 18:08:09 +0300

    feature: env global types

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('devteambg-notes', 'postgres', '2001godina', {
-  dialect: 'postgres',
-  host: 'localhost'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST
+  }
+);
 
 export default sequelize;
---
+++ server/src/global.d.ts
@@ -0,0 +1,12 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    TOKEN_SECRET: string;
+    DB_NAME: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_HOST: string;
+    DB_DIALECT: string;
+  }
+}
