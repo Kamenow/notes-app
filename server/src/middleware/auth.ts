755b3536a17bdd51ca467cceb0378802046b6e75
Kamenow <iavorkamenov777@gmail.com>
2024-05-01 11:57:09 +0300

    protected routes

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, RequestHandler, Response } from 'express';
+import jwt, { Secret } from 'jsonwebtoken';
+import { CustomRequest } from '../interfaces/Request';
+import User from '../models/User';
+
+export const verifyToken = (
+  // TODO: fix typeing
+  req: CustomRequest & Request & any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token)
+    return res
+      .status(401)
+      .send({ message: 'Access denied. No token provided.' });
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.TOKEN_SECRET as unknown as Secret
+    );
+    req.user = decoded as unknown as User;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: 'Invalid token.' });
+  }
+};
--- server/src/routes/notesRoute.ts
+++ server/src/routes/notesRoute.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as noteController from '../controllers/noteController';
+import { verifyToken } from '../middleware/auth';
 
 const router = express.Router();
 
-router.post('/create-note', noteController.createNote);
-router.delete('/:id', noteController.deleteNoteById);
-router.get('/:id', noteController.getNoteById);
-router.get('/user/:id', noteController.getAllNotesOfUser);
+router.post('/create-note', verifyToken, noteController.createNote);
+router.delete('/:id', verifyToken, noteController.deleteNoteById);
+router.get('/:id', verifyToken, noteController.getNoteById);
+router.get('/user/:id', verifyToken, noteController.getAllNotesOfUser);
 router.patch('/:id', verifyToken, noteController.updateNoteById);
 
 export default router;
